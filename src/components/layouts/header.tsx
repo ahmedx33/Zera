2ba6f8a444992be47deb9e0700754c7d203ff3fe
ahmedx33 <faresahmed51857@gmail.com>
2024-09-23 03:05:38 +0300

    init 🚀

---
+++ src/components/layouts/header.tsx
@@ -0,0 +1,15 @@
+
+export default function Header() {
+    return (
+        <header>
+            <h1>Header</h1>
+            <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </header>
+    )
+}
